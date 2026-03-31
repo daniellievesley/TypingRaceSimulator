8ba4459fadcbbb1368fed708257262256ec80f77
Daniel Lievesley <danieljlievesley@gmail.com>
2026-03-31 14:42:45 +0100

    Test using Main

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main (String[] args){
-        Typist t = new Typist ('A', "Daniel", 0.7);
-        System.out.println("Progress - " + t.getProgress());
-        t.typeCharacter();
-        System.out.println("Progress - " + t.getProgress());
-        t.typeCharacter();
-        System.out.println("Progress - " + t.getProgress());
+        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.7);
+        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.4);
+        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.3);
+        TypingRace game = new TypingRace(10);
+        game.addTypist(t1, 1);
+        game.addTypist(t2, 2);
+        game.addTypist(t3, 3);
+        game.startRace();
     }
 }

b53bea83557753d4599c91ce9f5da5814e38f570
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-06 13:28:37 +0100

    Adjust typing race settings

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main (String[] args){
-        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.7);
-        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.4);
-        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.3);
-        TypingRace game = new TypingRace(10);
+        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.07);
+        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.18);
+        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.12);
+        TypingRace game = new TypingRace(7);
         game.addTypist(t1, 1);
         game.addTypist(t2, 2);
         game.addTypist(t3, 3);

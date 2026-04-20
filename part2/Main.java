fd4e9f1c9f2cb5ef20294d59bf5623739efdb4db
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-20 14:02:46 +0100

    cards ready to be added to mainPanel, which is then added to frame

--- part2/Main.java
+++ part2/Main.java
@@ -7,9 +7,13 @@ public class Main {
     JFrame frame = new JFrame("Typing Race Simulator");
     CardLayout layout = new CardLayout();
     JPanel mainPanel = new JPanel(layout);
-    IntroPage introSetup = new IntroPage(layout, mainPanel);
-    RacePage raceArea = new RacePage(layout, mainPanel);
-    ResultsPage resultArea = new ResultsPage(layout, mainPanel);
-    mainPanel.add();
+    IntroPage introSetup = new IntroPage();
+    RacePage raceArea = new RacePage();
+    ResultsPage resultArea = new ResultsPage();
+    mainPanel.add(IntroPage, "INTRO");
+    mainPanel.add(RacePage, "RACE");
+    mainPanel.add(ResultsPage, "RESULTS");
+    frame.add(mainPanel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
 }

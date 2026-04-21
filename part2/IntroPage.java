b4b1ae4b225e6dd22ed7a585a3928fc61ea3712c
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 12:58:20 +0100

    adding each panel to card panel - using border layout, so that is is visible

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -19,6 +19,7 @@ public class IntroPage extends JPanel{
         passageLenBox.setEditable(true);
         passageChooser.add(passageLenBox);
         passageChooser.add(customText);
+        add(passageChooser, BorderLayout.WEST);
         // seat chooser panel
         JPanel seatChooser = new JPanel();
         seatChooser.setBackground(Color.PINK);
@@ -27,6 +28,7 @@ public class IntroPage extends JPanel{
         Integer [] noOfSeatsAvailable = { 2, 3, 4, 5, 6};
         JComboBox<Integer> seatsPicked = new JComboBox<>(noOfSeatsAvailable);
         seatChooser.add(seatsPicked);
+        add(seatChooser, BorderLayout.CENTER);
 
         // dificulty modifiers panel 
         JPanel modifiersPanel = new JPanel();
@@ -39,10 +41,12 @@ public class IntroPage extends JPanel{
         modifiersPanel.add(autoCorrect);
         modifiersPanel.add(caffeineMode);
         modifiersPanel.add(nightShift);
+        add(modifiersPanel, BorderLayout.EAST);
 
         // continue panel
         JPanel continuetoGamePanel = new JPanel();
         JButton continuetoGame = new JButton("Continue to game");
         continuetoGamePanel.add(continuetoGame);
+        add(modifiersPanel, BorderLayout.SOUTH);
     }
 }

1cd71c378c4ed3f7679cfe42bd653db4694c335b
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "Inserted combo box which takes from array to allow for passage length to be chosen by user"
    This reverts commit 3212054c709a92c44f3f063acd82a190ed9263a0.

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -10,18 +10,10 @@ public class IntroPage {
 
         JPanel panelTitle = new JPanel();
         JLabel titleText = new JLabel("Welcome to the Typing Race Simulator!");
-        frame.add(panelTitle, BorderLayout.NORTH);
+
         panelTitle.add(titleText);
-        // passage chooser panel
-        JPanel passageChooser = new JPanel();
-        JLabel passageLabelText = new JLabel("Choose pre-set passage length or insert");
-        frame.add(passageChooser, BorderLayout.CENTER);
-        passageChooser.add(passageLabelText);
-        String[] lengths = {"Short", "Medium", "Long", "Custom"};
-        JComboBox passageLenBox = new JComboBox<>(lengths);
-        passageLenBox.addItemListener(s);
-        passageLenBox.setEditable(true);
-        passageChooser.add(passageLenBox);
+
+        frame.add(panelTitle, BorderLayout.NORTH);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);

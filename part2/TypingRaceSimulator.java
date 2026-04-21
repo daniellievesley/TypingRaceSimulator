6031b39ad8baef2868e27c32998f7c056e90398e
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 13:03:02 +0100

    changes to create new card layout, with app card added to main app frame, which will be placeholder for game screen

--- part2/TypingRaceSimulator.java
+++ part2/TypingRaceSimulator.java
@@ -6,7 +6,14 @@ public class TypingRaceSimulator {
     public static void main(String[] args){
         JFrame mainApp = new JFrame("Typing Race Simulator");
         mainApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel appCard = new JPanel(new CardLayout());
+        CardLayout layout = new CardLayout();
+        JPanel appCard = new JPanel(layout);
         JPanel introCard = new IntroPage(appCard);
+        appCard.add(introCard, "Intro");
+        mainApp.add(appCard);
+        mainApp.pack();
+        mainApp.setLocationRelativeTo(null);
+        mainApp.setVisible(true);
+        layout.show(appCard, "Intro");
     }
 }

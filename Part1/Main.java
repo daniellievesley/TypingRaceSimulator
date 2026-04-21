aece3ab458e5ed2f71302fdff3e76dd7490c953b
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:21:04 +0100

    Revert "Testing different typists' accuracies"
    This reverts commit eb1ea856ee052bea988cb16229f5360fb16e2db4.

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main (String[] args){
-        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.01);
-        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.02);
-        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.03);
-        TypingRace game = new TypingRace(3);
+        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.07);
+        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.18);
+        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.12);
+        TypingRace game = new TypingRace(7);
         game.addTypist(t1, 1);
         game.addTypist(t2, 2);
         game.addTypist(t3, 3);

3f31e0577a3035004a3d9079983c675715f45587
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 17:13:47 +0100

    test data removed - system.out.println for debugging

--- part2/CustomiseTypists.java
+++ part2/CustomiseTypists.java
@@ -3,25 +3,26 @@ import java.awt.*;
 import javax.swing.*;
 
 public class CustomiseTypists extends JPanel {
-    Integer len;
+    String len;
     String passage;
     Integer seats;
-    Boolean auto;
-    Boolean caffeine;
-    Boolean night;
+    boolean auto;
+    boolean caffeine;
+    boolean night;
     public CustomiseTypists(JPanel mainPanel) {
         setLayout(new BorderLayout());
         JLabel label = new JLabel("Customising typists", SwingConstants.CENTER);
         add(label, BorderLayout.CENTER);
     }
 
-    public void takeData(Integer len, String passage, Integer seats, Boolean auto, Boolean caffeine, boolean night) {
+    public void takeData(String len, String passage, Integer seats, Boolean auto, Boolean caffeine, boolean night) {
         this.len = len;
         this.passage = passage;
         this.seats = seats;
         this.auto = auto;
         this.caffeine = caffeine;
         this.night = night;
+        
     }
 
     

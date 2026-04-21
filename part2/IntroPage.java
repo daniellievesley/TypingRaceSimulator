60249697d56768b460e2327bdbc1dc7f25460bed
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:21:04 +0100

    Revert "changes to constructor to pass class CustomiseTypists with methods (e.g takeData)"
    This reverts commit e1ead46148528ad30f363e67bbdaab5d2917f9c4.

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class IntroPage extends JPanel{
-    IntroPage(JPanel app, CustomiseTypists customiseCard){
+    IntroPage(JPanel app, JPanel customiseCard){
         setLayout(new BorderLayout());
         JPanel panelTitle = new JPanel();
         JLabel titleText = new JLabel("Welcome to the Typing Race Simulator!");

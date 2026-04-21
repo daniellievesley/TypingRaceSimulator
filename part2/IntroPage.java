7461c93431c2cbfb3d05f8237de5781bd7b17bb8
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "Adjusting the passage field JTextfield with column length of 20 so that it is wider to represent insertion of passage"
    This reverts commit bf1cc65c2ea61fb1f835520083e326124cc649a5.

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -19,7 +19,7 @@ public class IntroPage {
         passageChooser.add(passageLabelText);
         String[] lengths = {"Short", "Medium", "Long", "Custom"};
         JComboBox passageLenBox = new JComboBox<>(lengths);
-        JTextField customText = new JTextField("Passage here", 20);
+        JTextField customText = new JTextField("Enter custom passage, if appropriate");
         passageLenBox.setEditable(true);
         passageChooser.add(passageLenBox);
         passageChooser.add(customText);

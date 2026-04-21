e29f7389543659651b5b768b2b7f74a30b8322d9
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "Setting up frame with card layout of panels in main method as application"
    This reverts commit 7d8cb4727a21d4b812e6f545c59d418c792e5084.

--- part2/Main.java
+++ part2/Main.java
@@ -4,9 +4,16 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        JFrame mainApp = new JFrame("Typing Race Simulator");
-        mainApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel appCard = new JPanel(new CardLayout());
-        JPanel introCard = new IntroPage(appCard);
-    }
+    JFrame frame = new JFrame("Typing Race Simulator");
+    CardLayout layout = new CardLayout();
+    JPanel mainPanel = new JPanel(mainPanel, layout);
+    IntroPage introSetup = new IntroPage(mainPanel, layout);
+    RacePage raceArea = new RacePage(lmainPanel, ayout);
+    ResultsPage resultArea = new ResultsPage(layout);
+    mainPanel.add(IntroPage, "INTRO");
+    mainPanel.add(RacePage, "RACE");
+    mainPanel.add(ResultsPage, "RESULTS");
+    frame.add(mainPanel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }

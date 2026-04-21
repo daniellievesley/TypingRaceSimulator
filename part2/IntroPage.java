e1b71619ccdb3d300306d9b4d95b19b6ac3b2a9c
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "Added modifiers by implementing jcheckbox within jpanel"
    This reverts commit 64fd5d6ddc12c2467d945882522ac18e148b70de.

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -7,6 +7,7 @@ public class IntroPage {
         JFrame frame = new JFrame("Setup - Typing Race Simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
         JPanel panelTitle = new JPanel();
         JLabel titleText = new JLabel("Welcome to the Typing Race Simulator!");
         frame.add(panelTitle, BorderLayout.NORTH);
@@ -14,7 +15,7 @@ public class IntroPage {
         // passage chooser panel
         JPanel passageChooser = new JPanel();
         JLabel passageLabelText = new JLabel("Choose passage length");
-        frame.add(passageChooser, BorderLayout.EAST);
+        frame.add(passageChooser, BorderLayout.CENTER);
         passageChooser.add(passageLabelText);
         String[] lengths = {"Short", "Medium", "Long", "Custom"};
         JComboBox passageLenBox = new JComboBox<>(lengths);
@@ -29,9 +30,6 @@ public class IntroPage {
         Integer [] noOfSeatsAvailable = { 2, 3, 4, 5, 6};
         JComboBox seatsPicked = new JComboBox<>(noOfSeatsAvailable);
         seatChooser.add(seatsPicked);
-        frame.add(seatChooser, BorderLayout.CENTER);
-
-        // dificulty modifiers panel 
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);

b49419ca94ea35f31593fb130720c1962c5e2010
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "changes to create new card layout, with app card added to main app frame, which will be placeholder for game screen"
    This reverts commit 6031b39ad8baef2868e27c32998f7c056e90398e.

--- part2/TypingRaceSimulator.java
+++ part2/TypingRaceSimulator.java
@@ -6,14 +6,7 @@ public class TypingRaceSimulator {
     public static void main(String[] args){
         JFrame mainApp = new JFrame("Typing Race Simulator");
         mainApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        CardLayout layout = new CardLayout();
-        JPanel appCard = new JPanel(layout);
+        JPanel appCard = new JPanel(new CardLayout());
         JPanel introCard = new IntroPage(appCard);
-        appCard.add(introCard, "Intro");
-        mainApp.add(appCard);
-        mainApp.pack();
-        mainApp.setLocationRelativeTo(null);
-        mainApp.setVisible(true);
-        layout.show(appCard, "Intro");
     }
 }

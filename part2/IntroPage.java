2ed4511927409eaf4ba692989af31650e43eb6bb
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-21 19:22:58 +0100

    Revert "Added in data types as classes to JComboBoxes"
    This reverts commit beec110d1bfd467f484253dcb2d1d591de16ed9d.

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -18,7 +18,7 @@ public class IntroPage {
         frame.add(passageChooser, BorderLayout.EAST);
         passageChooser.add(passageLabelText);
         String[] lengths = {"Short", "Medium", "Long", "Custom"};
-        JComboBox<String> passageLenBox = new JComboBox<>(lengths);
+        JComboBox passageLenBox = new JComboBox<>(lengths);
         JTextField customText = new JTextField("Passage here", 20);
         passageLenBox.setEditable(true);
         passageChooser.add(passageLenBox);
@@ -29,7 +29,7 @@ public class IntroPage {
         JLabel seatChooserLabel = new JLabel("Number of seats");
         seatChooser.add(seatChooserLabel);
         Integer [] noOfSeatsAvailable = { 2, 3, 4, 5, 6};
-        JComboBox<Integer> seatsPicked = new JComboBox<>(noOfSeatsAvailable);
+        JComboBox seatsPicked = new JComboBox<>(noOfSeatsAvailable);
         seatChooser.add(seatsPicked);
         frame.add(seatChooser, BorderLayout.CENTER);
 
@@ -54,12 +54,5 @@ public class IntroPage {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-        // event listener when continue is clicked
-        continuetoGame.addActionListener(e -> moveToFeatures(frame));
-    }
-
-    public static void moveToFeatures(JFrame frame){
-        
     }
 }

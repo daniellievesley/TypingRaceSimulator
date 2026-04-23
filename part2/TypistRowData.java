a0cca2925d82037cf1c3142c5ea6fef36a25a5d6
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-23 11:51:10 +0100

    updated to allow typist obj row to be set without colour

--- part2/TypistRowData.java
+++ part2/TypistRowData.java
@@ -11,13 +11,17 @@ public class TypistRowData {
     JCheckBox energyDrink;
     JCheckBox headphones;
 
-    TypistRowData(JComboBox<String> typingStyle, JComboBox<String> keyboardType, JTextField symbol, Color colourChosen, JCheckBox wrist, JCheckBox energyDrink, JCheckBox headphones){
+    TypistRowData(JComboBox<String> typingStyle, JComboBox<String> keyboardType, JTextField symbol, JCheckBox wrist, JCheckBox energyDrink, JCheckBox headphones){
         this.typingStyle = typingStyle;
         this.keyboardType = keyboardType;
         this.symbol = symbol;
-        this.colourChosen = colourChosen;
+        this.colourChosen = Color.RED;
         this.wrist = wrist;
         this.energyDrink = energyDrink;
         this.headphones = headphones;
+    }
+
+    public void updateColour(Color color){
+        this.colourChosen = color;
     }
 }

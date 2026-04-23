93b3447e0fd55e90832475391b49786965c9f642
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-23 14:45:02 +0100

    if statement used to determine passage setting and set string accordingly

--- part2/RacePage.java
+++ part2/RacePage.java
@@ -9,6 +9,20 @@ public class RacePage extends JPanel {
     }
 
     public void passData (GameData gameinfo, List<TypistRowData> typistData){
-        System.out.println("Data received");
+        if (gameinfo.len.equals("Short")){
+            String passagetoSend = "The quick brown fox jumps over the lazy dog. It is often used to practice typing because it includes every letter.";
+        }
+        else if (gameinfo.len.equals("Medium")){
+            String passagetoSend = "Typing is a useful skill that improves with regular practice. By focusing on accuracy first, you can build a strong foundation and gradually increase your typing speed over time.";
+        }
+        else if (gameinfo.len.equals("Long")){
+            String passagetoSend = "Typing is an essential skill in today’s digital world, helping people communicate quickly and efficiently. With consistent practice and proper technique, you can improve both speed and accuracy, allowing your thoughts to flow more naturally as you type.";
+        }
+        else if (gameinfo.len.equals("Custom")){
+            String passagetoSend = gameinfo.passage;
+        }
+        
+
+        
     }
 }

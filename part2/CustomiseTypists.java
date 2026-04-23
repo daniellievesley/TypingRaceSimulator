7cf0ab66b035734daf9e3aade3c541fcf5acb41f
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-23 09:38:50 +0100

    not suitable for obj colour to setName

--- part2/CustomiseTypists.java
+++ part2/CustomiseTypists.java
@@ -51,7 +51,6 @@ public class CustomiseTypists extends JPanel {
             clrPickerReq.addActionListener(e -> {
                 JColorChooser clrChooser = new JColorChooser();
                 Color colorTypist = JColorChooser.showDialog(null, "Pick a typist colour", Color.blue);
-                colorTypist.setName("Colour");
                 System.out.println(colorTypist);
                 typistChildPanel.add(clrChooser);
             });

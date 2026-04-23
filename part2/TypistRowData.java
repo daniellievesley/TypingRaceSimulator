c4c3fe5c581e36c92a92aabf184b8ae2c62e6743
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-23 17:20:03 +0100

    created getter methods - encapsulation for all attributes

--- part2/TypistRowData.java
+++ part2/TypistRowData.java
@@ -3,13 +3,43 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TypistRowData {
-    JComboBox<String> typingStyle;
-    JComboBox<String> keyboardType;
-    JTextField symbol;
-    Color colourChosen;
-    JCheckBox wrist;
-    JCheckBox energyDrink;
-    JCheckBox headphones;
+    private JComboBox<String> typingStyle;
+    private JComboBox<String> keyboardType;
+    private JTextField symbol;
+    private Color colourChosen;
+    private JCheckBox wrist;
+    private JCheckBox energyDrink;
+    private JCheckBox headphones;
+
+    public JComboBox<String> getTypingStyle(){
+        return this.typingStyle;
+    }
+
+    public JComboBox<String> getKeyboardType(){
+        return this.keyboardType;
+    }
+
+    public JTextField getSymbol(){
+        return this.symbol;
+    }
+
+    public Color getColour(){
+        return this.colourChosen;
+    }
+
+    public JCheckBox getWrist(){
+        return this.wrist;
+    }
+
+    public JCheckBox getEnergy(){
+        return this.energyDrink;
+    }
+
+    public JCheckBox getHeadphones(){
+        return this.headphones;
+    }
+
+
 
     TypistRowData(JComboBox<String> typingStyle, JComboBox<String> keyboardType, JTextField symbol, JCheckBox wrist, JCheckBox energyDrink, JCheckBox headphones){
         this.typingStyle = typingStyle;

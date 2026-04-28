e667167557f841648c2d67450d1e7a9506ce5e44
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-28 19:14:38 +0100

    new constructor for leaderboard obj

--- part2/IntroPage.java
+++ part2/IntroPage.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class IntroPage extends JPanel{
-    IntroPage(JPanel app, CustomiseTypists customiseCard, RacePage racePage){
+    IntroPage(JPanel app, CustomiseTypists customiseCard, RacePage racePage, Leaderboard board){
         setLayout(new BorderLayout());
         JPanel panelTitle = new JPanel();
         JLabel titleText = new JLabel("Welcome to the Typing Race Simulator!");
@@ -59,7 +59,7 @@ public class IntroPage extends JPanel{
         Boolean auto = autoCorrect.isSelected();
         Boolean caffeine = caffeineMode.isSelected();
         Boolean night = nightShift.isSelected();
-        customiseCard.takeData(length, customPassage, seats, auto, caffeine, night, app, racePage);
+        customiseCard.takeData(length, customPassage, seats, auto, caffeine, night, app, racePage, board);
         layout.show(app, "CustomiseTypists");
         
     });

c7a9606e7168eaac82dfe67def58d0340736c80c
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-29 14:59:11 +0100

    accuracies for sample game (terminal)

--- Part1/Main.java
+++ Part1/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main (String[] args){
-        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.07);
-        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.18);
-        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.12);
+        Typist t1 = new Typist ('\u2460', "TurboTyper", 0.6);
+        Typist t2 = new Typist ('\u2461', "QwertyMaster", 0.8);
+        Typist t3 = new Typist ('\u2462', "QuickTyper", 0.4);
         TypingRace game = new TypingRace(7);
         game.addTypist(t1, 1);
         game.addTypist(t2, 2);

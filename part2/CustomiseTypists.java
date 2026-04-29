483460b7664feabea4162b6f71e2fa8504def60f
Daniel Lievesley <danieljlievesley@gmail.com>
2026-04-29 20:37:39 +0100

    comments on methods and storage lists

--- part2/CustomiseTypists.java
+++ part2/CustomiseTypists.java
@@ -38,13 +38,18 @@ public class CustomiseTypists extends JPanel {
      * @param caffeine contains the boolean value of if caffeine is selected
      * @param night contains boolean value of if night shift is selected
      * @param app contains the main panel which holds all cards of the game
-     * @param racePage contains
+     * @param racePage contains UI object where race will be shown
+     * @param board contains UI object where results will be shown
      */
     public void takeData(String len, String passage, Integer seats, Boolean auto, Boolean caffeine, boolean night, JPanel app, RacePage racePage, Leaderboard board) {
+        // gameInfo object created to store game data selected by user, easier to handle settings
         GameData gameInfo = new GameData(len, passage, seats, auto, caffeine, night);
+        // list created to store references to each typists' panel - housing individual customisation input areas via UI
         List<JPanel> typistPanels = new ArrayList<>();
+        // list created to store objects of TypistData - holding individual customisations
         List<TypistRowData> typistData = new ArrayList<>();
         for (int i=1; i<=seats; i++){
+            // new typist child panel created for every seat needed
             JPanel typistChildPanel = new JPanel();
             JLabel typistLabel = new JLabel("Typist " + i);
             typistLabel.setFont(typistLabel.getFont().deriveFont(Font.BOLD));
@@ -73,20 +78,24 @@ public class CustomiseTypists extends JPanel {
             typistChildPanel.add(energyDrink);
             JCheckBox ncHeadphones = new JCheckBox("Noise-cancelling headphones");
             typistChildPanel.add(ncHeadphones);
+            // new object created with references to each input field (JTextField, JCheckBox etc )
             TypistRowData rowofData = new TypistRowData(name, typingStyle, keyboardType, symbol, wristSupport, energyDrink, ncHeadphones);
             typistData.add(rowofData);
+            // action listener on colour request btn to allow for colour pop up - this allows for colour to be chosen PER typist
             clrPickerReq.addActionListener(e -> {
                 JColorChooser clrChooser = new JColorChooser();
-                Color colorTypist = JColorChooser.showDialog(null, "Pick a typist colour", Color.blue);
+                Color colorTypist = JColorChooser.showDialog(null, "Pick a typist colour", Color.blue); // default selected colour is blue
                 System.out.println(colorTypist);
                 rowofData.updateColour(colorTypist);
                 typistChildPanel.add(clrChooser);
             });
 
         } 
+        // start game button created to pass to next UI card
         JButton contToGame = new JButton("Start game now");
         add(contToGame);
 
+        // action listener on start game btn to pass data and show next UI 
         contToGame.addActionListener(e -> {
             CardLayout layout = (CardLayout) app.getLayout();
             racePage.passData(gameInfo, typistData, app, board);
